1a94a4f938f90065a287621dd8bfb13c67cf8c20
spadea96334 <16223709+spadea96334@users.noreply.github.com>
2020-09-17 15:19:29 +0800

    change base view use player not songlist

--- AmpachePlayer/SongCell.swift
+++ AmpachePlayer/SongCell.swift
@@ -3,10 +3,25 @@ import UIKit
 
 class SongCell: UITableViewCell {
     
+    public var song: SongModel? {
+        didSet {
+            self.updateSongInfo()
+        }
+    }
+    
     @IBOutlet weak var titleLabel: UILabel!
     
-    public func setTitle(title: String) {
-        self.titleLabel.text = title
+    override func prepareForReuse() {
+        self.song = nil
+        self.titleLabel.text = ""
+    }
+    
+    func updateSongInfo() {
+        if self.song == nil {
+            return
+        }
+        
+        self.titleLabel.text = self.song!.title
     }
     
     required init?(coder: NSCoder) {
---
+++ AmpachePlayer/SongListViewController.swift
@@ -0,0 +1,41 @@
+import Foundation
+import UIKit
+
+class SongListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var SongTableView: UITableView!
+    
+    override func viewDidLoad() {
+        NotificationCenter.default.addObserver(self, selector: #selector(onSongListChanged), name: NSNotification.Name(rawValue: "SongListChanged"), object: nil)
+    }
+    
+    @objc func onSongListChanged() {
+        DispatchQueue.main.async {
+            self.SongTableView.reloadData()
+        }
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return AmpacheManager.sharedInstance.songList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SongCell")!
+        
+        if let songCell = cell as? SongCell {
+            songCell.song = AmpacheManager.sharedInstance.songList[indexPath.row]
+        }
+        
+        return cell
+    }
+}
