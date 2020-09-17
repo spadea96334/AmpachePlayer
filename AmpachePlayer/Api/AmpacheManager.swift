0d9ff3c14d39d1098a513c4d118ed0ec9d58a899
spadea96334 <16223709+spadea96334@users.noreply.github.com>
2020-09-17 11:27:50 +0800

    using json data save last login info

--- AmpachePlayer/Api/AmpacheManager.swift
+++ AmpachePlayer/Api/AmpacheManager.swift
@@ -54,35 +54,20 @@ class AmpacheManager: NSObject {
     }
     
     func saveLoginInfo() {
-        UserDefaults.standard.set(self.handshakeModel?.auth, forKey: "auth")
-        UserDefaults.standard.set(self.handshakeModel?.api, forKey: "api")
-        UserDefaults.standard.set(self.handshakeModel?.session_expire, forKey: "session_expire")
-        UserDefaults.standard.set(self.handshakeModel?.update, forKey: "update")
-        UserDefaults.standard.set(self.handshakeModel?.clean, forKey: "clean")
-        UserDefaults.standard.set(self.handshakeModel?.songs, forKey: "songs")
-        UserDefaults.standard.set(self.handshakeModel?.albums, forKey: "albums")
-        UserDefaults.standard.set(self.handshakeModel?.artists, forKey: "artists")
-        UserDefaults.standard.set(self.handshakeModel?.playlists, forKey: "playlists")
-        UserDefaults.standard.set(self.handshakeModel?.videos, forKey: "videos")
-        UserDefaults.standard.set(self.handshakeModel?.catalogs, forKey: "catalogs")
+        let data = try? JSONEncoder().encode(self.handshakeModel)
         
-        UserDefaults.standard.synchronize()
+        if data != nil {
+            UserDefaults.standard.set(data, forKey: "handshake")
+            UserDefaults.standard.synchronize()
+        } else {
+            NSLog("Can't encode handshake model")
+        }
     }
     
     func loadLoginInfo() {
-        var model: HandshakeModel = HandshakeModel.init(auth: "", api: "", session_expire: "", update: "", add: "", clean: "", songs: 0, albums: 0, artists: 0, playlists: 0, videos: 0, catalogs: 0)
-        model.auth =  UserDefaults.standard.value(forKey: "api") as! String
-        model.api =  UserDefaults.standard.value(forKey: "auth") as! String
-        model.session_expire =  UserDefaults.standard.value(forKey: "session_expire") as! String
-        model.update =  UserDefaults.standard.value(forKey: "update") as! String
-        model.clean =  UserDefaults.standard.value(forKey: "clean") as! String
-        model.songs =  UserDefaults.standard.value(forKey: "songs") as! Int
-        model.albums =  UserDefaults.standard.value(forKey: "albums") as! Int
-        model.artists =  UserDefaults.standard.value(forKey: "artists") as! Int
-        model.playlists =  UserDefaults.standard.value(forKey: "playlists") as! Int
-        model.videos =  UserDefaults.standard.value(forKey: "videos") as! Int
-        model.catalogs =  UserDefaults.standard.value(forKey: "catalogs") as! Int
-        
+        guard let data = UserDefaults.standard.value(forKey: "handshake") as? Data else {return}
+        guard let model = try? JSONDecoder().decode(HandshakeModel.self, from: data) else {return}
+           
         if !model.isEmpty() {
             self.isLogin = true
             self.handshakeModel = model
