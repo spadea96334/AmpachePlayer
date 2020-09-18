2d30cac657f16999b2540d5c350e28bc034e5a87
spadea96334 <16223709+spadea96334@users.noreply.github.com>
2020-09-18 18:12:13 +0800

    use art image on player background

--- AmpachePlayer/UI/PlayerViewController.swift
+++ AmpachePlayer/UI/PlayerViewController.swift
@@ -7,6 +7,7 @@ class PlayerViewController: UIViewController {
     @IBOutlet weak var artImageView: UIImageView!
     @IBOutlet weak var songTitleLabel: UILabel!
     @IBOutlet weak var playButton: PlayButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
     
     override func viewDidLoad() {
         NotificationCenter.default.addObserver(self, selector: #selector(onCurrentSongChanged), name: NSNotification.Name(rawValue: "CurrentSongChanged"), object: nil)
@@ -24,6 +25,7 @@ class PlayerViewController: UIViewController {
             DispatchQueue.main.async {
                 let artImage = image == nil ? UIImage.init(named: "blank_album") : image
                 self.artImageView.image = artImage
+                self.backgroundImageView.image = artImage
             }
         }
     }
