f45b92eb6fc63665379baaee92b05b7da9da3440
spadea96334 <16223709+spadea96334@users.noreply.github.com>
2020-10-14 16:51:29 +0800

    queue media self instead of using queue of AVQueuePlayer

--- AmpachePlayer/Audio/AudioPlayer.swift
+++ AmpachePlayer/Audio/AudioPlayer.swift
@@ -11,9 +11,10 @@ class AudioPlayer: NSObject {
     }
     private(set) var mediaList: [MediaModel] = []
     
+    let QUEUE_LENGTH = 2
     var currentMediaIndex = -1
     var avAudioPlayer = AVQueuePlayer.init()
-    
+
     public func addObserverForPlayerState(_ observer: NSObject){
         self.avAudioPlayer.addObserver(observer, forKeyPath: "timeControlStatus", options: NSKeyValueObservingOptions.new, context: nil)
     }
@@ -35,45 +36,26 @@ class AudioPlayer: NSObject {
     }
     
     public func addMedia(mediaList: [MediaModel]) {
-        // Temporarily disable this function ,because AVQueuePlayer stuck when adding a lot of item
-        // Todo: Implement a queue to replace queue of AVQueuePlayer
-        return
-        for media in mediaList {
-            guard let url = URL.init(string: media.url) else { continue }
-            let item = AVPlayerItem.init(url: url)
-            
-            if !self.avAudioPlayer.canInsert(item, after: nil) {
-                // Todo: show error
-            }
-            
-            self.mediaList.append(media)
-            self.avAudioPlayer.insert(item, after: nil)
-        }
+        self.mediaList += mediaList
         
-        // if this is the first media, start playing
-        if self.avAudioPlayer.items().count == 1 {
+        _ = self.fillItemToAudioPlayer()
+        
+        // If there is no playing media now, start playing
+        if self.avAudioPlayer.items().count != 0 && self.avAudioPlayer.timeControlStatus != .playing {
             self.avAudioPlayer.play()
-            
             // todo: remove this after ui finish
             self.avAudioPlayer.volume = 0.2
         }
     }
     
     public func addMedia(media: MediaModel) {
-        guard let url = URL.init(string: media.url) else { return }
-        let item = AVPlayerItem.init(url: url)
-        
-        if !self.avAudioPlayer.canInsert(item, after: nil) {
-            // Todo: show error
-        }
-        
         self.mediaList.append(media)
-        self.avAudioPlayer.insert(item, after: nil)
         
-        // if this is the first media, start playing
-        if self.avAudioPlayer.items().count == 1 {
+        _ = self.fillItemToAudioPlayer()
+        
+        // If there is no playing media now, start playing
+        if self.avAudioPlayer.items().count != 0 && self.avAudioPlayer.timeControlStatus != .playing {
             self.avAudioPlayer.play()
-            
             // todo: remove this after ui finish
             self.avAudioPlayer.volume = 0.2
         }
@@ -86,6 +68,43 @@ class AudioPlayer: NSObject {
         self.mediaList = []
     }
     
+    func fillItemToAudioPlayer() -> Bool {
+        // is last media
+        guard self.currentMediaIndex < self.mediaList.count - 1 else {
+            return false
+        }
+        
+        // queue is full
+        if self.avAudioPlayer.items().count >= QUEUE_LENGTH {
+            return false
+        }
+        
+        let remainMedia = self.mediaList.count - self.currentMediaIndex - 1
+        // Append media until avplayer's item count equal to QUEUE_LENGTH or no media
+        for i in self.currentMediaIndex + 1...self.currentMediaIndex + min(QUEUE_LENGTH, remainMedia) {
+            let mediaModel = self.mediaList[i]
+            guard let mediaUrl = URL.init(string: mediaModel.url) else { return false }
+            let item = AVPlayerItem.init(url: mediaUrl)
+            
+            guard self.avAudioPlayer.canInsert(item, after: self.avAudioPlayer.currentItem) else {
+                return false
+            }
+            
+            self.avAudioPlayer.insert(item, after: self.avAudioPlayer.currentItem)
+        }
+
+        return true
+    }
+    
+    func onCurrentItemChanged() {
+        let mediaItems = self.avAudioPlayer.items()
+        
+        if mediaItems.count < QUEUE_LENGTH {
+            self.currentMediaIndex += 1
+            _ = self.fillItemToAudioPlayer()
+        }
+    }
+    
     override init() {
         super.init()
         self.avAudioPlayer.addObserver(self, forKeyPath: "currentItem", options: NSKeyValueObservingOptions.new, context: nil)
@@ -93,8 +112,7 @@ class AudioPlayer: NSObject {
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == "currentItem" {
-            guard self.currentMediaIndex != self.mediaList.count - 1 else { return }
-            self.currentMediaIndex += 1
+            self.onCurrentItemChanged()
             NotificationCenter.default.post(name: NSNotification.Name(rawValue: "CurrentMediaChanged"), object: nil)
         }
     }
